a7fb7fce0f44a61300b8579f843b1944096af819
neonspectrum <youngskymann@gmail.com>
2018-11-18 17:38:14 +0800

    if admin show college list

--- resources/views/modal.blade.php
+++ resources/views/modal.blade.php
@@ -3,14 +3,18 @@
     <div class="modal-content">
       <h4>Add</h4>
       <div class="row">
-        <div class="input-field col s12">
-          <p class="caption">College</p>
-          <select name="college">
-            @foreach(\App\Roles::all()->slice(1) as $role)
-              <option value="{{ $role->name }}" data-icon="{{ asset('public/img/logo/' . $role->logo) }}">{{ $role->description }}</option>
-            @endforeach
-          </select>
-        </div>
+        @if(Auth::user()->role == "admin")
+          <div class="input-field col s12">
+            <p class="caption">College</p>
+            <select name="college">
+              @foreach(\App\Roles::all()->slice(1) as $role)
+                <option value="{{ $role->name }}" data-icon="{{ asset('public/img/logo/' . $role->logo) }}">{{ $role->description }}</option>
+              @endforeach
+            </select>
+          </div>
+        @else
+          <input type="hidden" name="college" value="{{Auth::user()->role}}">
+        @endif
         <div class="input-field col s12">
           <p class="caption">Title</p>
           <input name="title" type="text" class="validate" placeholder="Enter the title" required>

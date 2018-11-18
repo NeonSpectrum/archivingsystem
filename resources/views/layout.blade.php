25a592f093aee038bfab533d34448d0edee12675
neonspectrum <youngskymann@gmail.com>
2018-11-18 16:51:39 +0800

    oops

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,7 +4,7 @@
   <base href="{{ url('/') . '/' }}">
   <meta charset="UTF-8">
   <meta name="csrf-token" content="{{ csrf_token() }}">
-  <link rel="stylesheet" href="{{ asset('public/css/packages.css') }}">
+  <link rel="stylesheet" href="{{ asset('public/css/packages.css') }}?v={{ filemtime(public_path('css/packages.css')) }}">
   <link rel="stylesheet" href="{{ asset('public/css/app.css') }}?v={{ filemtime(public_path('css/app.css')) }}">
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
   <title>Archiving System</title>
@@ -12,7 +12,7 @@
 <body class="{{ Auth::user()->role ?? '' }}">
   @yield("body")
 
-  <script src="{{ asset('public/js/packages.js') }}"></script>
+  <script src="{{ asset('public/js/packages.js') }}?v={{ filemtime(public_path('js/packages.js')) }}"></script>
   <script src="{{ asset('public/js/app.js') }}?v={{ filemtime(public_path('js/app.js')) }}"></script>
   @yield("extra-scripts")
 </body>

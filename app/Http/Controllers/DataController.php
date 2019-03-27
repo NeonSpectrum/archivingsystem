9de3fd7a8db3466e8add096a0a2e5b82910430cb
NeonSpectrum <youngskymann@gmail.com>
2019-03-27 23:29:46 +0000

    Update DataController.php

--- app/Http/Controllers/DataController.php
+++ app/Http/Controllers/DataController.php
@@ -200,7 +200,9 @@ class DataController extends Controller {
   /**
    * @return mixed
    */
-  protected function pdf(Request $request) {
+  protected function pdf(Request $request)
+    ini_set('max_execution_time', 0);
+    ini_set('memory_limit', '1024M');
     if ($request->pdf_data) {
       $request->pdf_data = json_decode($request->pdf_data);
 
